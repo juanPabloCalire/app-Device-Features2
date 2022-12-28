e2afb1520a7ed12d6e17e27fbb02ee2053390cb5
juanPabloCalire <juanpablocalire@gmail.com>
2022-12-28 15:44:09 -0300

    device-Features-2

---
+++ src/store/reducers/places.reducer.js
@@ -0,0 +1,27 @@
+import { ADD_PLACE } from "../actions/places.actions";
+import Place from "../../models/Place";
+
+const initialState = {
+  places: [],
+};
+
+ const PlacesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      const newPlace = new Place(
+        Date.now(),
+        action.payload.title,
+        action.payload.image,
+        action.payload.address,
+        action.payload.lat,
+        action.payload.lng
+      );
+      return {
+        ...state,
+        places: state.places.concat(newPlace),
+      };
+    default:
+      return state;
+  }
+ };
+export default PlacesReducer;
